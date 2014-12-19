39c1d039bb3365b946a1e36afbea76c1cfa06c25
Santosh Kumar <santosh79@gmail.com>
2014-12-19 10:07:03 -0500

    fixed couple minor bugs

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
-var jsdom         = require("jsdom");
-var fs            = require('fs');
-var path          = require('path');
-var request       = require("request");
-var _             = require("underscore");
-var SITE_DIR      = "./site";
-var BASE_URL      = process.env['BASE_URL'];
-var LINKS_VISITED = {};
+var jsdom        = require("jsdom");
+var fs           = require('fs');
+var path         = require('path');
+var request      = require("request");
+var _            = require("underscore");
+var BASE_URL     = process.env['BASE_URL'];
 var BROKEN_LINKS = [];
 
+function addToSet(set, element) {
+  return _.chain(set).reject(function(el) {
+    return el === element;
+  }).concat([element]).value();
+}
+
+function isInSet(set, element) {
+  return !!(_.find(set, function(el) {
+    return el === element;
+  }));
+}
+
+function isNotInSet(set, element) {
+  return !isInSet(set, element);
+}
+
 (function startScraping() {
-  var doScrape = function(LINKS) {
+  var doScrape = function(LINKS, LINKS_VISITED) {
     if (LINKS.length === 0) {
-      printResults();
+      printResults(LINKS_VISITED);
       return;
     }
     var url = LINKS.pop();
-    if(LINKS_VISITED[url]) {
+    if(isInSet(LINKS_VISITED, url)) {
       console.log('skipping url ' + url);
-      doScrape(LINKS);
+      doScrape(LINKS, LINKS_VISITED);
       return;
     }
     console.log('looking at url ' + url);
@@ -25,60 +39,63 @@ var BROKEN_LINKS = [];
     console.log(LINKS);
     console.log('LINKS_VISITED is ');
     console.log(LINKS_VISITED);
-    scrapeFrom(url, function(links) {
-      LINKS_VISITED[url] = true;
-      doScrape(LINKS.concat(links));
+    scrapeFrom(url, LINKS_VISITED, function(links) {
+      doScrape(LINKS.concat(links), addToSet(LINKS_VISITED, url));
       return;
     });
   };
-  doScrape([BASE_URL]);
+  doScrape([BASE_URL], {});
 }());
 
-function printResults() {
+function printResults(LINKS_VISITED) {
   console.log('scraping done');
-  console.log(_.keys(LINKS_VISITED));
+  console.log(LINKS_VISITED);
   console.log('broken links are');
   console.log(_.uniq(BROKEN_LINKS));
   process.exit(0);
 }
 
-function scrapeFrom(url, cb) {
+function scrapeFrom(url, LINKS_VISITED,  cb) {
   var fullUrl = url;
   if (url.substring(0, BASE_URL.length) !== BASE_URL) {
     fullUrl = BASE_URL + url;
   }
   request(fullUrl, function(err, res, body) {
-    LINKS_VISITED[url] = true;
     if(err || res.statusCode !== 200) {
       console.error('ERROR fetching url ' + url + " err " + err + ' status_code ' + res.statusCode);
       BROKEN_LINKS.push(url);
       cb([]);
+      return;
     }
-    extractLinksFrom(url, function(links) {
+    extractLinksFrom(url, LINKS_VISITED, function(links) {
       cb(links);
+      return;
     });
   });
 }
 
-function extractLinksFrom(url, cb) {
+function extractLinksFrom(url, LINKS_VISITED, cb) {
   jsdom.env(
     url,
     ["http://code.jquery.com/jquery.js"],
     function (errors, window) {
       var allAnchorTagsInPage = _.uniq(_.map(window.$("a"), function(link) { return link['href']; }));
-      var allImageTags        = _.uniq(_.map(window.$("img"), function(img) { return link['src']; }));
+      var allImageTags        = _.uniq(_.map(window.$("img"), function(link) { return link['src']; }));
       var allLinkTags         = _.uniq(_.map(window.$("link"), function(link) { return link['href']; }));
       var allScriptTags       = _.uniq(_.map(window.$("script"), function(link) { return link['src']; }));
       var linksAndScriptTags = allLinkTags.concat(allScriptTags);
       var allResourceLinks    = allAnchorTagsInPage.concat(allImageTags).concat(linksAndScriptTags);
-      var linksThatAreSelfHosted = _.filter(allResourceLinks, function(link) {
+      var linksThatAreSelfHosted = _.chain(allResourceLinks).filter(function(link) {
         return link.substring(0, BASE_URL.length) === BASE_URL || link[0]  === '/';
-      });
+      }).reject(function(link) {
+        return link.substring(0, 2) === '//';
+      }).value();
 
       var linksNotVisited = _.filter(linksThatAreSelfHosted, function(link) {
-        return !LINKS_VISITED[link];
+        return isNotInSet(LINKS_VISITED, link);
       });
       cb(linksNotVisited);
+      return;
     }
   );
 }
